b7f7273fd3af473df08a6d86415588f767be915d
hexlet_art <hexlet_art@mail.ru>
2022-04-08 16:47:04 +0300

    add option and arg-s for <filepath1> and <filepath2> by -f or --format <type> flag. finish aim (step 2.0)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('3.4.4');
+  .version('3.4.4')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
  
-program.parse();
+  program.parse(process.argv);
