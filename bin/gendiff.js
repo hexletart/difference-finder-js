60235c1d3b971b78b459c8033dda57a159a7870f
hexlet_art <hexlet_art@mail.ru>
2022-04-13 19:17:57 +0300

    add gendiff realisation

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { Command } from "../node_modules/commander/esm.mjs";
+import genDiff from "../src/index.js";
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('3.4.4')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
- 
-  program.parse(process.argv);
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => genDiff(filepath1, filepath2))
+  .parse(); // .parse(process.argv);
