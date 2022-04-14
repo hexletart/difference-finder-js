11d91b1f0699ebc691f4fafc6f16b3c9b0fc27ef
hexlet_art <hexlet_art@mail.ru>
2022-04-14 17:32:44 +0300

    modification of printing result on the screen

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,5 +10,5 @@ program
   .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => genDiff(filepath1, filepath2))
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
   .parse(); // .parse(process.argv);
