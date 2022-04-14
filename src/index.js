4206859afffbfbf736ce3ad11b976f66205c7fae
hexlet_art <hexlet_art@mail.ru>
2022-04-14 17:34:45 +0300

    revert to return output in gendiff function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import fileData from './data.js';
 
-export default (file1, file2, filler = ' ', fillerCount = 2) => {
+export default (filepath1, filepath2, filler = ' ', fillerCount = 2) => {
+  const [file1, file2] = [fileData(filepath1), fileData(filepath2)];
   const [file1Keys, file2Keys] = [Object.keys(file1), Object.keys(file2)];
   const uniqKeys = _.uniq([file1Keys, file2Keys].flat()).sort();
   const currentFiller = filler.repeat(fillerCount);
@@ -18,8 +19,6 @@ export default (file1, file2, filler = ' ', fillerCount = 2) => {
     };
     return getJsonPart(key);
   }).flat();
-  
   const currentLines = lines.map((line) => `${currentFiller}${line.sign} ${line.info}`);
-  const difference = ['{', ...currentLines, '}',].join('\n');
-  console.log(difference);
+  return ['{', ...currentLines, '}',].join('\n');
 };
