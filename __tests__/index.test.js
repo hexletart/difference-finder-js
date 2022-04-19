f7d46d60957dd7ca8ba71ff3a515d0cdff2834fa
hexlet_art <hexlet_art@mail.ru>
2022-04-19 13:30:02 +0300

    fix code by eslint notes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,10 @@
 import gendiff from '../src/index.js';
 import { getFixturePath, readFile } from '../src/data.js';
 
-test('test of gendiff', () => {
-  const fileName1 = 'file1.json';
-  const fileName2 = 'file2.json';
+test('test_of_gendiff', () => {
+  const file1 = 'file1.json';
+  const file2 = 'file2.json';
   const filesDifference = 'filesDiff.txt';
-  expect(gendiff(getFixturePath(fileName1), getFixturePath(fileName2))).toEqual(readFile(getFixturePath('filesDiff.txt')));
+  expect(gendiff(getFixturePath(file1), getFixturePath(file2)))
+    .toEqual(readFile(getFixturePath(filesDifference)));
 });
