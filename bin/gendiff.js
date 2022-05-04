fc6f7ff5e65996321028c06e35f6f28450fe3dc4
hexlet_art <hexlet_art@mail.ru>
2022-05-04 21:16:20 +0300

    reconstuction the main function introducing formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,9 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('3.4.4')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+  .action((filepath1, filepath2) => console
+    .log(genDiff(filepath1, filepath2, program.opts().format)))
   .parse();
