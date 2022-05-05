e36b91f901a92d61b6f5f558418532c595a21205
hexlet_art <hexlet_art@mail.ru>
2022-05-05 16:52:16 +0300

    fix codeclimate issue of too many returns (attempt 9)

--- src/index.js
+++ src/index.js
@@ -17,30 +17,6 @@ const stylish = (inputTree, format, dash = ' ', dashLength = 2) => {
   return null;
 };
 
-const buildBranchByKey = (file1, file2, uniqKey, callback) => {
-  const isInFirst = _.has(file1, uniqKey);
-  const isInSecond = _.has(file2, uniqKey);
-  const value1 = _.get(file1, uniqKey);
-  const value2 = _.get(file2, uniqKey);
-  if (_.isUndefined(file2) && _.isObject(file1)) return [{ sign: ' ', key: uniqKey, value: callback(value1) }];
-  let str;
-  if (_.has(file1, uniqKey) && !isInSecond) {
-    str = [{ sign: '-', key: uniqKey, value: value1 }];
-  } else if (isInSecond && !isInFirst) {
-    str = [{ sign: '+', key: uniqKey, value: value2 }];
-  } else if (_.isEqual(value1, value2)) {
-    str = [{ sign: ' ', key: uniqKey, value: value1 }];
-  } else {
-    str = (_.isObject(value1) && _.isObject(value2))
-      ? { sign: ' ', key: uniqKey, value: callback(value1, value2) }
-      : [
-        { sign: '-', key: uniqKey, value: callback(value1) },
-        { sign: '+', key: uniqKey, value: callback(value2) },
-      ];
-  }
-  return str;
-};
-
 export default (filepath1, filepath2, format = 'stylish') => {
   const [fileData1, fileData2] = [filepath1, filepath2]
     .map((path) => getDataByParcing(getValidPath(path)));
@@ -51,7 +27,25 @@ export default (filepath1, filepath2, format = 'stylish') => {
     const objKeys = [file1, file2]
       .filter((data) => _.isObject(data)).map((data) => Object.keys(data)).flat();
     const uniqKeys = _.uniq(objKeys).sort();
-    return uniqKeys.map((key) => buildBranchByKey(file1, file2, key, diffIdent)).flat();
+    const getTree = uniqKeys.map((key) => {
+      const buildBranchByKey = (uniqKey) => {
+        const isInFirst = _.has(file1, uniqKey);
+        const isInSecond = _.has(file2, uniqKey);
+        const value1 = _.get(file1, uniqKey);
+        const value2 = _.get(file2, uniqKey);
+        if (_.isUndefined(file2) && _.isObject(file1)) return [{ sign: ' ', key: uniqKey, value: diffIdent(value1) }];
+        if (isInFirst && !isInSecond) return [{ sign: '-', key: uniqKey, value: diffIdent(value1) }];
+        if (isInSecond && !isInFirst) return [{ sign: '+', key: uniqKey, value: diffIdent(value2) }];
+        if (_.isEqual(value1, value2)) return [{ sign: ' ', key: uniqKey, value: diffIdent(value1) }];
+        return (_.isObject(value1) && _.isObject(value2)) ? { sign: ' ', key: uniqKey, value: diffIdent(value1, value2) }
+          : [
+            { sign: '-', key: uniqKey, value: diffIdent(value1) },
+            { sign: '+', key: uniqKey, value: diffIdent(value2) },
+          ];
+      };
+      return buildBranchByKey(key);
+    }).flat();
+    return getTree;
   };
   const filesDiff = diffIdent(fileData1, fileData2);
   return stylish(filesDiff, format);
