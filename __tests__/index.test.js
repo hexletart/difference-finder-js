38b6b415f929f16e78db363f40bb5ec5890ca2fe
hexlet_art <hexlet_art@mail.ru>
2022-05-10 13:06:24 +0300

    change arguments positions to solve the issue

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,26 +2,28 @@ import { readFileSync } from 'fs';
 import genDiff from '../src/index';
 import { getFixturePath as getPath } from '../src/data/filePath';
 
+const defaultFormatValue = 'stylish';
+
 test('empty_values_for_genDiff', () => {
-  expect(genDiff(null, getPath(null))).toBe(null);
+  expect(genDiff(defaultFormatValue, null, getPath(null))).toBe(null);
 });
 
 test('format_mix_check_for_gendiff', () => {
   const first = 'jsonFile1.json';
   const second = 'yamlFile2.yml';
   const expected = 'stylishDiffOfTwo.txt';
-  expect(genDiff(getPath(first), getPath(second)))
+  expect(genDiff(defaultFormatValue, getPath(first), getPath(second)))
     .toEqual(readFileSync(getPath(expected), 'utf-8'));
 });
 
 test('wrong_format_for_parsers', () => {
   const first = 'stylishDiffOfTwo.txt';
-  expect(genDiff(getPath(first))).toBe(null);
+  expect(genDiff(defaultFormatValue, getPath(first))).toBe(null);
 });
 
 test('not_valid_value_for_formatter', () => {
   const first = 'yamlFile1.yaml';
   const second = 'jsonFile2.json';
   const format = 'format';
-  expect(genDiff(getPath(first), getPath(second), format)).toBe(null);
+  expect(genDiff(format, getPath(first), getPath(second))).toBe(null);
 });
