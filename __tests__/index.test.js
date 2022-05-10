3edc0e5a387e11e2e7b28b2810e0da9acb8e56ad
hexlet_art <hexlet_art@mail.ru>
2022-05-10 12:10:53 +0300

    test reconstraction by new logic

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,16 +2,21 @@ import { readFileSync } from 'fs';
 import genDiff from '../src/index';
 import { getFixturePath as getPath } from '../src/data/filePath';
 
-test('empty_test_of_genDiff', () => {
+test('empty_values_for_genDiff', () => {
   expect(genDiff(null, getPath(null))).toBe(null);
 });
 
-test('not_valid_format_for_gendiff', () => {
-  const first = 'jsonFile2.json';
-  const second = 'diffTestJson1Yaml2.txt';
-  const expected = 'diffTestJson2Txt.txt';
+test('format_mix_check_for_gendiff', () => {
+  const first = 'jsonFile1.json';
+  const second = 'yamlFile2.yml';
+  const expected = 'stylishDiffOfTwo.txt';
   expect(genDiff(getPath(first), getPath(second)))
     .toEqual(readFileSync(getPath(expected), 'utf-8'));
+});
+
+test('wrong_format_for_parsers', () => {
+  const first = 'stylishDiffOfTwo.txt';
+  expect(genDiff(getPath(first))).toBe(null);
 });
 
 test('not_valid_value_for_formatter', () => {
