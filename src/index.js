02b476f0c0e987d869045c2086d2574e6a938a85
hexlet_art <hexlet_art@mail.ru>
2022-05-10 12:17:27 +0300

    function reconstaction by new approach which was mentioned by step 6 of project but it had been missed in the beginning of the leg

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import { getValidPath } from './data/filePath.js';
 import getDataByParcing from './data/parsers.js';
 
 const stylish = (inputTree, format, dash = ' ', dashLength = 2) => {
-  const buildStruct = (branches, depth = 1) => {
+  const getStylishOutput = (tree, depth = 1) => {
     const currentGap = (level) => dash.repeat(dashLength * level);
     const bracketGap = (level) => dash.repeat((dashLength * level) - dashLength);
-    const lines = branches.map((branch) => {
-      const [prefix, name, children] = Object.keys(branch).map((el) => branch[el]);
-      const currentChildren = (_.isArray(children)) ? buildStruct(children, depth + 2) : children;
-      return `${currentGap(depth)}${prefix} ${name}: ${currentChildren}`;
-    });
+    const lines = tree.reduce((acc, branch) => {
+      const [branchData, prefix = ' '] = [branch].flat();
+      const linesChunk = Object.entries(branchData)
+        .map(([key, value]) => {
+          const currentValue = (_.isObject(value))
+            ? getStylishOutput([value].flat(), depth + 2) : value;
+          return `${currentGap(depth)}${prefix} ${key}: ${currentValue}`;
+        });
+      return [...acc, ...linesChunk];
+    }, []);
     return ['{', ...lines, `${bracketGap(depth)}}`].join('\n');
   };
-  if (format === 'stylish') return buildStruct(inputTree);
+  if (format === 'stylish') return getStylishOutput(inputTree);
   return null;
 };
 
@@ -21,30 +26,30 @@ export default (filepath1, filepath2, format = 'stylish') => {
   const [fileData1, fileData2] = [filepath1, filepath2]
     .map((path) => getDataByParcing(getValidPath(path)));
   if (!fileData1 && !fileData2) return null;
-
   const diffIdent = (file1, file2) => {
-    if (_.isUndefined(file2) && !_.isObject(file1)) return file1;
+    if (_.isUndefined(file2) || _.isEqual(file1, file2)) return file1;
     const objKeys = [file1, file2]
       .filter((data) => _.isObject(data)).map((data) => Object.keys(data)).flat();
     const uniqKeys = _.uniq(objKeys).sort();
-    const getTree = uniqKeys.map((key) => {
+    const getTree = uniqKeys.flatMap((key) => {
       const buildBranchByKey = (uniqKey) => {
         const isInFirst = _.has(file1, uniqKey);
         const isInSecond = _.has(file2, uniqKey);
         const value1 = _.get(file1, uniqKey);
         const value2 = _.get(file2, uniqKey);
-        if (_.isUndefined(file2) && _.isObject(file1)) return [{ sign: ' ', key: uniqKey, value: diffIdent(value1) }];
-        if (isInFirst && !isInSecond) return [{ sign: '-', key: uniqKey, value: diffIdent(value1) }];
-        if (isInSecond && !isInFirst) return [{ sign: '+', key: uniqKey, value: diffIdent(value2) }];
-        if (_.isEqual(value1, value2)) return [{ sign: ' ', key: uniqKey, value: diffIdent(value1) }];
-        return (_.isObject(value1) && _.isObject(value2)) ? { sign: ' ', key: uniqKey, value: diffIdent(value1, value2) }
-          : [
-            { sign: '-', key: uniqKey, value: diffIdent(value1) },
-            { sign: '+', key: uniqKey, value: diffIdent(value2) },
-          ];
+        if (isInFirst && !isInSecond) return [{ [uniqKey]: diffIdent(value1) }, '-'];
+        if (isInSecond && !isInFirst) return [{ [uniqKey]: diffIdent(value2) }, '+'];
+        if ((!_.isObject(value1) || !_.isObject(value2)) && !_.isEqual(value1, value2)) {
+          return [
+            [{ [uniqKey]: diffIdent(value1) }, '-'],
+            [{ [uniqKey]: diffIdent(value2) }, '+'],
+          ].flat();
+        }
+        return [{ [uniqKey]: diffIdent(value1, value2) }, ' '];
       };
-      return buildBranchByKey(key);
-    }).flat();
+      const treeData = (buildBranchByKey(key));
+      return _.chunk(treeData, 2);
+    });
     return getTree;
   };
   const filesDiff = diffIdent(fileData1, fileData2);
