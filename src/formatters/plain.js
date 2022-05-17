465b4dd44b3320dcbff7b3a9c789d4347d974aab
hexlet_art <hexlet_art@mail.ru>
2022-05-17 21:47:38 +0300

    elimination of "let"

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,47 +1,43 @@
 import _ from 'lodash';
 
 export default (inputTree) => {
-  const buildString = (layout, action, ...options) => {
+  const buildString = (layout, notice, ...options) => {
     const [option1, option2] = options.filter((el) => !_.isUndefined(el))
       .map((el) => ((typeof el === 'string') ? `'${el}'` : el))
       .map((el) => (_.isObject(el) ? `${'[complex value]'}` : el));
     const prefix = `Property '${layout}' was`;
-    let body;
-    switch (action) {
-      case 'removed': body = 'removed';
-        break;
-      case 'added': body = `added with value: ${option1}`;
-        break;
-      case 'updated': body = `updated. From ${option1} to ${option2}`;
-        break;
-      default: body = '. Something went wrong';
-    }
+    const getBody = (action) => {
+      switch (action) {
+        case 'removed': return 'removed';
+        case 'added': return `added with value: ${option1}`;
+        case 'updated': return `updated. From ${option1} to ${option2}`;
+        default: return '. Something went wrong';
+      }
+    };
+    const body = getBody(notice);
     return `${prefix} ${body}`;
   };
   const buildComparison = (tree, root = []) => {
     const branchesData = tree.map((branch) => branch[_.head(Object.keys(branch))]);
     const keys = branchesData.map((branch) => Object.keys(branch)).flat();
     const branches = tree.map((branch) => {
-      const [branchData, sign] = Object.keys(branch).map((key) => branch[key]);
+      const [branchData, mark] = Object.keys(branch).map((key) => branch[key]);
       const leafs = Object.entries(branchData)
         .map(([key, value]) => {
           const newRoot = [...root, key];
           const rootForPrint = newRoot.join('.');
           const isUniq = keys.filter((el) => _.isEqual(el, key)).length === 1;
-          let printCall;
-          if (sign === '-' && isUniq) {
-            printCall = buildString(rootForPrint, 'removed');
-          } else if (sign === '-' && !isUniq) {
-            const [value1, value2] = branchesData.map((el) => _.get(el, key, [])).flat();
-            printCall = (buildString(rootForPrint, 'updated', value1, value2));
-          } else if (sign === '+' && isUniq) {
-            printCall = buildString(rootForPrint, 'added', value);
-          } else if (sign === ' ') {
-            printCall = (_.isArray(value)) ? buildComparison(value, newRoot) : [];
-          } else {
-            printCall = [];
-          }
-          return printCall;
+          const getStaitment = (sign, status) => {
+            if (sign === '-' && status) return buildString(rootForPrint, 'removed');
+            if (sign === ' ') return (_.isArray(value)) ? buildComparison(value, newRoot) : [];
+            if (sign === '+' && status) return buildString(rootForPrint, 'added', value);
+            if (sign === '-' && !status) {
+              const [value1, value2] = branchesData.map((el) => _.get(el, key, [])).flat();
+              return (buildString(rootForPrint, 'updated', value1, value2));
+            }
+            return [];
+          };
+          return getStaitment(mark, isUniq);
         }).flat();
       return leafs;
     }).flat();
