76ce5d208ed954f5ac323f8c811c5843a6d3c6ba
hexlet_art <hexlet_art@mail.ru>
2022-05-17 22:19:11 +0300

    add get back another default to fix issue

--- src/data/filePath.js
+++ src/data/filePath.js
@@ -5,5 +5,5 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const getValidPath = (ident) => ((ident) ? path.resolve(process.cwd(), ident) : `${ident}`);
-export const getFixturePath = (ident) => ((ident) ? path.join(__dirname, '../..', '__fixtures__', ident) : `${ident}`);
+export const getValidPath = (ident) => ((ident) ? path.resolve(process.cwd(), ident) : null);
+export const getFixturePath = (ident) => ((ident) ? path.join(__dirname, '../..', '__fixtures__', ident) : null);
