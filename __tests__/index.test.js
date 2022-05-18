57ce8d34432e95dc921ac7a1bddf1d3fbb2eeb2c
hexlet_art <hexlet_art@mail.ru>
2022-05-18 07:17:01 +0300

    tests reduction

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,15 +4,18 @@ import { getFixturePath as getPath } from '../src/data/filePath';
 
 const defaultFormatValue = 'stylish';
 
+const json1 = 'jsonFile1.json';
+const json2 = 'jsonFile2.json';
+const yaml1 = 'yamlFile1.yaml';
+const yaml2 = 'yamlFile2.yml';
+
 test('empty_values_for_genDiff', () => {
   expect(genDiff(null, getPath(null), defaultFormatValue)).toBe(null);
 });
 
 test('mix_check_for_gendiff_by_default_format', () => {
-  const first = 'jsonFile1.json';
-  const second = 'yamlFile2.yml';
   const expected = 'stylishDiffOfTwo.txt';
-  expect(genDiff(getPath(first), getPath(second), defaultFormatValue))
+  expect(genDiff(getPath(json1), getPath(yaml2)))
     .toEqual(readFileSync(getPath(expected), 'utf-8'));
 });
 
@@ -23,26 +26,20 @@ test('wrong_format_for_parsers', () => {
 });
 
 test('not_valid_value_for_formatter', () => {
-  const first = 'yamlFile1.yaml';
-  const second = 'jsonFile2.json';
   const format = 'format';
-  expect(genDiff(getPath(first), getPath(second), format)).toBe(null);
+  expect(genDiff(getPath(yaml1), getPath(json2), format)).toBe(null);
 });
 
 test('yaml_check_for_gendiff_by_plain_format', () => {
-  const first = 'yamlFile1.yaml';
-  const second = 'yamlFile2.yml';
   const format = 'plain';
   const expected = 'plainDiffOfTwo.txt';
-  expect(genDiff(getPath(first), getPath(second), format))
+  expect(genDiff(getPath(yaml1), getPath(yaml2), format))
     .toEqual(readFileSync(getPath(expected), 'utf-8'));
 });
 
 test('json_check_for_gendiff_by_json_format', () => {
-  const first = 'jsonFile1.json';
-  const second = 'jsonFile2.json';
   const format = 'json';
   const expected = 'jsonDiffOfTwo.txt';
-  expect(genDiff(getPath(first), getPath(second), format))
+  expect(genDiff(getPath(json1), getPath(json2), format))
     .toEqual(readFileSync(getPath(expected), 'utf-8'));
 });
