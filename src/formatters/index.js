a9d74105706c89e6f4f1bca7608fc57fdd964d43
hexlet_art <hexlet_art@mail.ru>
2022-05-18 06:45:13 +0300

    add default back to find issue

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -7,6 +7,6 @@ export default (treeData, format) => {
     case 'stylish': return getStylishOutput(treeData);
     case 'plain': return getPlainOutput(treeData);
     case 'json': return getJsonOutput(treeData);
-    default: return `${JSON.stringify(treeData)}${'\n'}${format}`;
+    default: return null;
   }
 };
